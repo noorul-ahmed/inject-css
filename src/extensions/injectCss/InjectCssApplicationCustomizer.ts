1a626c7813814525f35417ab055a8ed1c7d29906
Noorul Ahmed <noorul.ahmed@gmail.com>
2022-06-13 21:07:31 +0200

    Commit

---
+++ src/extensions/injectCss/InjectCssApplicationCustomizer.ts
@@ -0,0 +1,44 @@
+import { override } from '@microsoft/decorators';
+import { Log } from '@microsoft/sp-core-library';
+import {
+  BaseApplicationCustomizer
+} from '@microsoft/sp-application-base';
+import { Dialog } from '@microsoft/sp-dialog';
+
+import * as $ from 'jquery';
+import * as strings from 'InjectCssApplicationCustomizerStrings';
+
+const LOG_SOURCE: string = 'InjectCssApplicationCustomizer';
+
+/**
+ * If your command set uses the ClientSideComponentProperties JSON input,
+ * it will be deserialized into the BaseExtension.properties object.
+ * You can define an interface to describe it.
+ */
+export interface IInjectCssApplicationCustomizerProperties {
+  // This is an example; replace with your own property
+  cssurl: string;
+}
+
+/** A Custom Action which can be run during execution of a Client Side Application */
+export default class InjectCssApplicationCustomizer
+  extends BaseApplicationCustomizer<IInjectCssApplicationCustomizerProperties> {
+
+  @override
+  public onInit(): Promise<void> {
+    Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
+
+    const cssUrl: string = this.properties.cssurl;
+    if (cssUrl) {
+      // inject the style sheet
+      const head: any = document.getElementsByTagName("head")[0] || document.documentElement;
+      let customStyle: HTMLLinkElement = document.createElement("link");
+      customStyle.href = cssUrl;
+      customStyle.rel = "stylesheet";
+      customStyle.type = "text/css";
+      head.insertAdjacentElement("beforeEnd", customStyle);
+    }
+
+    return Promise.resolve();
+  }
+}
